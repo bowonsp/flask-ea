ffe6c5d6c823be5f229597eb2c19b2771d6bf748
bowonsp <sempurnakulit@gmail.com>
2025-07-23 13:02:24 +0700

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,15 +1,14 @@
 def generate_signal(data):
-    candles = data['candles']
-
+    candles = data.get("candles", [])
     if len(candles) < 2:
-        return {'signal': 'HOLD'}
+        return {"signal": "HOLD"}
 
-    last_close = candles[-1]['close']
-    prev_close = candles[-2]['close']
+    last_close = candles[-1]["close"]
+    prev_close = candles[-2]["close"]
 
     if last_close > prev_close:
-        return {'signal': 'BUY'}
+        return {"signal": "BUY", "tp": last_close + 10, "sl": last_close - 10}
     elif last_close < prev_close:
-        return {'signal': 'SELL'}
+        return {"signal": "SELL", "tp": last_close - 10, "sl": last_close + 10}
     else:
-        return {'signal': 'HOLD'}
+        return {"signal": "HOLD"}
